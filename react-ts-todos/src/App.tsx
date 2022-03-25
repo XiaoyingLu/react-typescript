26ca98573fa50136dff06834d0bff041f17d7eab
Amber Lu <amber.lu@ubisoft.com>
2022-03-25 12:44:53 -0300

    remove a todo item

--- react-ts-todos/src/App.tsx
+++ react-ts-todos/src/App.tsx
@@ -5,18 +5,24 @@ import Todo from "./models/todo";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  
+
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
     setTodos((prevTodos) => {
       return prevTodos.concat(newTodo);
-    })
-  }
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
 
   return (
     <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
